e1f6ec0a24273b5f25355952127a3169e8e39c46
Yosef Dejene <121252303+Yosef7474@users.noreply.github.com>
2025-11-14 16:53:50 +0300

    Time: 75 ms (67.42%) | Memory: 33.9 MB (40.88%) - LeetSync

---
+++ solutions/2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        players.sort()
+        trainers.sort()
+
+        counter = 0
+
+        p = 0
+        t = 0
+
+
+        while p < len(players) and t < len(trainers):
+            if players[p] <= trainers[t]:
+                counter+=1
+                p+=1
+                t+=1
+            else:
+                t+=1
+
+        return counter
+
+        
